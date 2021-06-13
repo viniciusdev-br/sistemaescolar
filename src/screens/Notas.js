45f764e5c05a2ce4d0b7147d245df0b70cd73d0e
Vinicius Botelho <viniciusvcb2002@gmail.com>
2021-06-13 16:57:03 -0300

    Tabelas funcionando no Firestore

--- src/screens/Notas.js
+++ src/screens/Notas.js
@@ -6,67 +6,58 @@ import {Table, Row,TableWrapper, Rows, Cell, Col, Cols} from 'react-native-table
 export default function Notas({route}){
   const userId = route.params.user;
   //state de um vetor para receber disciplinas do Firestore
-  const [disciplinas, setDisciplinas] = useState('[]');
+  const [disciplinas, setDisciplinas] = useState([]);
+  const [ano, setAno]= useState('1ano');
+  const [turma,setTurma] = useState('EN01208');
+  const [tituloTurma,setTituloTurma] = useState('');
+  const [nome, setNome] = useState('');
+  const [firtsAvaliacao, setFirstAvaliation] = useState([]);
+  const [secondAvaliacao, setSecondAvaliation] = useState([]);
+  const [thirdAvaliacao, setThirdAvaliation] = useState([]);
+  const [forAvaliacao, setForAvaliacao] = useState([]);
+  //const cursos = ['Portugues', 'matematica', 'Fisíca', 'Quimica', 'Biologia','Portugues', 'matematica', 'Fisíca', 'Quimica', 'Biologia','Portugues', 'matematica', 'Fisíca', 'Quimica', 'Biologia']
+  /*const tableData = [
+    ['1', '2', '3','10'],
+    ['1', '2', '3','10'],
+    ['1', '2', '3','10'],
+    ['1', '2', '3','10'],
+    ['1', '2', '3','10'],
+    ['1', '2', '3','10'],
+    ['1', '2', '3','10'],
+    ['1', '2', '3','10'],
+    ['1', '2', '3','10'],
+    ['1', '2', '3','10'],
+  ];*/
 
+  useEffect(()=>{
+    firestore().collection("users").doc(userId).onSnapshot( doc => {
+      setAno(doc.data().ano);
+      setTurma(doc.data().turma);
+      setNome(doc.data().nome);
+      setFirstAvaliation(doc.data().firtsAvaliacao);
+      setSecondAvaliation(doc.data().secondAvaliacao);
+    });
+    firestore().collection(ano).doc(turma).onSnapshot( doc => {
+      setTituloTurma(doc.data().titleTurma);
+      setDisciplinas(doc.data().disciplinas);
+    })
+  });
   const tableHead = ['Disciplinas', '1ª', '2ª', '3ª','4ª'];
-  const cursos = ['Portugues', 'matematica', 'Fisíca', 'Quimica', 'Biologia','Portugues', 'matematica', 'Fisíca', 'Quimica', 'Biologia','Portugues', 'matematica', 'Fisíca', 'Quimica', 'Biologia']
-  const firstAvaliation = ['2','1', '2', '3', '4'];
-  const tableData = [
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-    ['1', '2', '3','10'],
-  ];
-
-//  useEffect(()=>{
-//    firestore().collection("users").doc(userId).onSnapshot( doc => {
-//      
-//    });
-//  });
-
-  /*const cursos = [
-    {name: 'Português', nota: 0},
-    {name: 'Matenática', nota: 1},
-    {name: 'Física', nota: 2},
-    {name: 'Química', nota: 3},
-    {name: 'Biologia', nota: 4},
-    {name: 'História', nota: 5},
-    {name: 'Geografia', nota: 6},
-    {name: 'Ciênc. da Religião', nota: 7},
-    {name: 'Ed. Física', nota: 8},
-    {name: 'Informática', nota: 9},
-  ]*/
-
-  function User({nameCurso}, {notaCurso}) {
-    return(
-      <View style={styles.item}>
-        <Text style={styles.texto}>{nameCurso}: {notaCurso}</Text>
-      </View>
-    );
-  }
-
   return(
   <SafeAreaView style={styles.container}>
     
     <ScrollView>
+      <Text style={styles.titleAluno}>Turma {tituloTurma} | {nome}</Text>
       <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
-        <Row data={tableHead} flexArr={[1.35, 1, 1, 1]}  style={styles.head} textStyle={{fontWeight: 'bold', textAlign: 'center', color:'#2E2FBF'}} />
+        <Row data={tableHead} style={styles.head} textStyle={{fontWeight: 'bold', textAlign: 'center', color:'#2E2FBF'}} />
         <TableWrapper style={styles.wrapper}>
-
-          <Col data={cursos} style={styles.title}  textStyle={styles.text}/>
-          <Rows data={tableData} flexArr={[1, 1, 1]} style={styles.row} textStyle={styles.text}/>
-
+          {/*Coluna com disciplinas*/}
+          <Col data={disciplinas} style={styles.title}  textStyle={styles.text}/>
+          {/*Colunas para cada avaliação do aluno*/}
+          <Col data={firtsAvaliacao} style={styles.row} textStyle={styles.text}/>
+          <Col data={secondAvaliacao} style={styles.row} textStyle={styles.text}/>
+          <Col data={disciplinas} style={styles.row} textStyle={styles.text}/>
+          <Col data={disciplinas} style={styles.row} textStyle={styles.text}/>
         </TableWrapper>
 
       </Table>
@@ -78,31 +69,28 @@ export default function Notas({route}){
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      marginTop: 15,
+      marginTop: 0,
     },
-    item: {
-      backgroundColor: '#85B2DD',
-      padding: 13,
+    titleAluno: {
+      alignSelf: 'center',
       marginVertical: 8,
-      marginHorizontal: 16,
-      borderRadius: 5,
+      fontSize: 18,
+      color:'#85B2DD',
+      fontWeight:'bold',
     },
     wrapper: { 
       flexDirection: 'row' 
     },
     title: { 
       flex: 1, 
-      backgroundColor: '#f6f8fa' ,
+      backgroundColor: '#f6f8fa',
+      height: '100%',
     },
     row: {  
-      height: 40  
+      height: '100%',
     },
     text: { 
       textAlign: 'center',
-    },
-    texto: {
-      fontSize: 20,
-      color: 'white',
     },
     headCursos: {  
       width: 100,
